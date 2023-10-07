34c6c8725ed4b87b540545b2f8009eb672461b23
jacare da nubank <141188220+mthy41@users.noreply.github.com>
2023-10-07 20:16:32 -0300

    commit

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import datetime
 
 
 #Estrutura de dados:
-#"user":["senha", "nome completo", "cargo", [ids de noticias publicadas]]
+#"user":["senha", "nome completo", "cargo", [ids de noticias curtidas], [ids de noticias publicadas]]
 #o [ids de noticias publicadas] apenas para usuarios do cargo ADM
 
 userlist = {
     "guest": ["guest", "convidado", "GUEST"],
-    "cat": ["123", "Miguel Ohara", "ADM", []],
-    "rat":["123", "Kimberlie Schwatson", "USER"],
-    "dog":["123", "Miles Morales", "USER"],
-    "bird":["123", "Ivan Neves", "USER"]
+    "cat": ["123", "Miguel Ohara", "ADM", [],[]],
+    "rat":["123", "Kimberlie Schwatson", "USER",[]],
+    "dog":["123", "Miles Morales", "USER", []],
+    "bird":["123", "Ivan Neves", "USER",[]]
 }
 
-newsdict = {}
+newsdict = {
+    "1": ["title", "body", "author", "date", 3, []],
+    "2": ["title2", "body2", "author2", "date2", 5, []],
+    "3": ["title3", "body3", "author3", "date3", 1, []]
+}
 
 comlist = {
     "comID":["comAuthor", "comText"]
@@ -25,7 +29,84 @@ def dateandtime ():
     format = "%Y-%m-%d %H:%M:%S"
     currentTime = now.strftime(format)
     return(currentTime)
-    
+
+
+def viewnews (userinput, userchoice):
+    newsvalues = newsdict[userchoice]
+    uservalues = userlist[userinput]
+    likedNewsIDs = uservalues[3]
+    isliked = False
+    while True:
+        
+        if userchoice in likedNewsIDs:
+            isliked = True
+            likeopt = "[1]Remover curtida."
+        else:
+            isliked = False
+            likeopt = "[1]Curtir notícia."
+        print(
+            f"{likedNewsIDs}\n"
+            f"{newsvalues[0]}\n" #Tutulo
+            f"{newsvalues[1]}\n" #Texto
+            f" ❤ {newsvalues[4]} | Publicado por @{newsvalues[2]} | {newsvalues[3]}\n" #Curtidas, Autor, Data
+            "--------------------------------------------\n"
+            f"{likeopt}\n"
+            "[2]Comentar na notícia.\n"
+            "[3]Ver comentários\n"
+            "[0]Sair."
+        )
+        userviewchoice = str(input(">> "))
+        
+        #Curtir ou remover curtida
+        if (userviewchoice == "1"):
+            if (isliked == False):
+                (likedNewsIDs.append(userchoice))
+                uservalues.insert(3,likedNewsIDs)
+                newsvalues[4] += 1
+                #isliked = True
+                continue
+            if (isliked == True):
+                likedNewsIDs = uservalues[3]
+                likedNewsIDs.remove(userchoice)
+                uservalues.insert(3, likedNewsIDs)
+                newsvalues[4] -= 1
+                continue
+        if (userviewchoice == "0"):
+            newsdict[userchoice] = newsvalues
+            userlist[userinput] = uservalues
+            break
+                
+
+
+def listnews (userinput):
+    for i in range (1, (len(newsdict) + 1)):
+        newsvalues = newsdict[str(i)]
+        print(
+            f"ID[{i}] {newsvalues[0]}:\n" # ID e Titulo
+            #f"{newsvalues[1]}\n" #Texto
+            f" ❤ {newsvalues[4]} | Publicado por @{newsvalues[2]} | {newsvalues[3]}\n"
+            "--------------------------------------------"
+        )
+    print(
+        "Digite o [ID] para ver a notícia.\n"
+        "[*]Para reverter ordem da lista\n"
+        "[0]Para voltar."
+    )
+    userchoice = input(">> ")
+    if (userchoice in newsdict):
+        viewnews(userinput, userchoice)
+
+   
+def recentnews ():
+    newsvalues = newsdict[str(len(newsdict))]
+    print(
+        "Notícia mais recente:\n"
+        f"ID[{len(newsdict)}] {newsvalues[0]}:\n" #Titulo
+        f"{newsvalues[1]}\n" #Texto
+        f" ❤ {newsvalues[4]} | Publicado por @{newsvalues[2]} | {newsvalues[3]}\n"
+        "--------------------------------------------"
+    )
+
 
 def postnew (userinput):
     title = str(input("Insira o título:\n>> "))
@@ -65,14 +146,16 @@ def postnew (userinput):
 
         #Publicar
         if (userchoicepostnew == "3"):
+            likes = 0
             newsid = str(len(newsdict) + 1)
-            newsvalues = [title, body, (userlist[userinput][1]),(dateandtime()), []]
+            newsvalues = [title, body, (userinput),(dateandtime()), likes, []]
             newsdict[newsid] = newsvalues
             print(newsdict)
 
 
 def logged (userinput="guest"):
     while True:
+        recentnews()
         print (f"Bem vindo(a) {userlist[userinput][1]}")
         print(
             "[0]Deslogar.\n"
@@ -98,14 +181,21 @@ def logged (userinput="guest"):
         if (userloggedchoice == "0"):
             break
         
+        if userlist[userinput][2] == "USER":
+            if (userloggedchoice == "2"):
+                listnews(userinput)
+        
         if userlist[userinput][2] == "ADM":
+            if (userloggedchoice == "2"):
+                listnews(userinput)
             if (userloggedchoice == "4"):
                 postnew(userinput)
 
 
 def main ():
     while True:
-        print("Menu\nBem vindo, selecione sua opção:")
+        recentnews()
+        print("Bem vindo, selecione sua opção:")
         menuchoice = str(input(
             "[1]Fazer login.\n"
             "[2]Cadastrar.\n"
@@ -122,9 +212,9 @@ def main ():
                 upi = getpass.getpass("Digite sua senha:\n>> ")
                 
                 if (userinput in userlist):
-                    print("o usuario existe")
+                    #print("o usuario existe")
                     if (upi in userlist[userinput][0]):
-                        print('a senha existe')
+                        #print('a senha existe')
                         logged(userinput)
                         break
                     else:
