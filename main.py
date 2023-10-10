2740f32a2e46d421989133553f5d5932b58cc7d5
jacare da nubank <141188220+mthy41@users.noreply.github.com>
2023-10-09 21:34:57 -0300

    new features
    more

--- main.py
+++ main.py
@@ -256,6 +256,45 @@ def postnew (userinput):
             break
 
 
+def searchnews (userinput):
+    while True:
+        verspace()
+        for i in range (1, (len(newsdict) + 1)):
+            newsvalues = newsdict[str(i)]
+            print(
+                f"ID[{i}] {newsvalues[0]}:\n" # ID e Titulo
+                #f"{newsvalues[1]}\n" #Texto
+                f" ❤ {newsvalues[4]} | Publicado por @{newsvalues[2]} | {newsvalues[3]}\n"
+                f"{horbar(horbar)}"
+            )
+        print(
+        "Selecione uma opção.\n"
+        "[1]Procurar por ID.\n"
+        "[2]Procurar por título.\n"
+        "[0]Sair."
+    )
+        searchnewschoice = input("")
+        
+        if (searchnewschoice == "0"):
+            break
+        if (searchnewschoice == "1"):
+            idsearch = input("ID: ")
+            if (idsearch in newsdict):
+                viewnews(userinput, idsearch)
+            else:
+                print("ID inválido ou notícia não existe.")
+                input("Pressione enter para continuar")
+            continue
+        if (searchnewschoice == 2):
+            titlesearch = input("Título: ")
+            for i in newsdict:
+                values = newsdict[i]
+                if (titlesearch == values[0]):
+                    viewnews(userinput, i)
+                    continue
+            input("Notícia não encontrada.\nPressione enter para continuar.")
+
+
 def logged (userinput="guest"):
     while True:
         verspace()
@@ -287,11 +326,23 @@ def logged (userinput="guest"):
         if (userloggedchoice == "0"):
             verspace()
             break
+
+        if userlist[userinput][1] == "GUEST":
+            if (userloggedchoice == "2"):
+                searchnews(userinput)
+        
+        if userlist[userinput][1] == "USER":
+            if (userloggedchoice == "2"):
+                searchnews(userinput)
+                
+        if userlist[userinput][2] == "ADM":
+            if (userloggedchoice == "1"):
+                searchnews(userinput)
         
         if userlist[userinput][2] == "GUEST":
             if (userloggedchoice == "2"):
                 listnews(userinput)
-        
+
         #Opções de usuário
         if userlist[userinput][2] == "USER":
             if (userloggedchoice == "2"):
@@ -304,6 +355,101 @@ def logged (userinput="guest"):
             if (userloggedchoice == "4"):
                 postnew(userinput)
 
+def sigin():
+    user = ""
+    username = ""
+    userage = ""
+    userrole = ""
+    userpassword = ""
+    while True:
+        verspace()
+        print(
+            f"Usuário: {user}\n"
+            f"Nome completo: {username}\n"
+            f"Idade: {userage}\n"
+            #f"Cargo: {userrole}\n"
+        )
+        if (userrole == ""):
+            print("Nessa plataforma, leitores podem visualizar, pesquisar e interagir com as notícias publicadas.\nJá os Editores, além de serem leitores, podem publicar e editar notícias.\n")
+            userrolechoice = str(input("Você será leitor ou editor?\n[1]Leitor.\n[2]Editor.\n[0]Voltar ao menu principal.\n>> "))
+            if (userrolechoice == "0"):
+                break
+            elif (userrolechoice == "1"):
+                userrole = "USER"
+                continue
+            elif (userrolechoice == "2"):
+                userrole = "ADM"
+                continue
+            else:
+                input("Opção inválida. Pressione enter para continuar.\n>> ")
+                userrole = ""
+                continue
+            
+                
+        if (user == ""):
+            user = str(input("Insira o nome de usuário:\n>> "))
+            if (user in userlist):
+                print("O usuário inserido já está em uso.")
+                user = ""
+                input("Pressione enter para continuar.")
+                continue
+            continue #
+        if (username == ""):
+            firstusername = str(input("Insira o seu primeiro nome:\n>> "))
+            if (firstusername == "" or len(firstusername) <= 2):
+                print("Primeiro nome inválido. Pressione enter para continaur.")
+                input()
+                continue
+            lastusername = str(input("Insira o seu sobrenome:\n>> "))
+            if (lastusername == "" or len(lastusername) == 2):
+                print("Primeiro nome inválido. Pressione enter para continaur.")
+                input()
+                continue
+            username = firstusername.capitalize() + " " + lastusername.capitalize()
+            continue
+        if (userage == ""):
+            userage = input("Insira a sua idade:\n>> ")
+            try:
+                userage = int(userage)
+            except ValueError:
+                print("Idade inválida. Pressione enter para continuar.")
+                input()
+                userage = ""
+                continue
+            if (userage < 18):
+                print("Você não tem idade suficiente para fazer uma conta.")
+                input(">> ")
+                break
+            continue
+        
+        if (userpassword == ""):
+            passwordone = getpass.getpass("Insira uma nova senha.\n>> ")
+            if len(passwordone) <= 5:
+                input("Sua senha precisa ter pelo menos 6 caracteres.\nPressione enter para continuar.\n>> ")
+                continue
+            passwordtwo = getpass.getpass("Verifique a senha.\n>> ")
+            if (passwordone != passwordtwo):
+                input("Senhas não batem.\nPressione enter para continuar.\n>> ")
+                continue
+            userpassword = passwordone
+            continue
+                
+                        
+        signinchoice = input(
+            "[1]Confirmar cadastro.\n"
+            "[0]Cancelar e sair.\n"
+        )
+        if (signinchoice == "0"):
+            break
+        if (signinchoice == "1"):
+#"guest": ["guest", "convidado", "GUEST",[]]
+            if (userrole == "USER"):
+                userlist[len(userlist)+1] = [userpassword, username, userrole, []]
+                break
+            if (userrole == "ADM"):
+                userlist[user] = [userpassword, username, userrole, [],[]]
+                break               
+
 
 def main ():
     while True:
@@ -314,6 +460,7 @@ def main ():
             "[2]Cadastrar.\n"
             "[3]Entrar como convidado.\n"
             "[0]Sair.\n"
+            #f"{userlist}"
         ))
         #Sair
         if (menuchoice == "0"):
@@ -332,15 +479,27 @@ def main ():
                         logged(userinput)
                         break
                     else:
-                        print('A senha digitada está incorreta. Tente novamente.')
-                        continue
+                        print("Usuário não existe ou o senha está incorreta.\nPressione enter ou insira [0] para voltar ao início.")
+                        c = input(">> ")
+                        if (c == "0"):
+                            break
+                        else:
+                            continue
                     
                 else:
-                    print("O login não existe ou o usuário está incorreto. Tente novamente.")
-                    continue
+                    print("Usuário não existe ou o senha está incorreta.\nPressione enter ou insira [0] para voltar ao início.")
+                    c = input(">> ")
+                    if (c == "0"):
+                        break
+                    else:
+                        continue
         #Login como convidado
         if (menuchoice == "3"):
             logged()
+            
+        if (menuchoice == "2"):
+            sigin()
+            
             
 
 main()
